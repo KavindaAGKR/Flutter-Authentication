e5b867a03f77adc72bdf5ed1fb6af550f77041e3
KavindaAGKR <kavindaaludeniya@gmail.com>
2024-10-08 12:54:45 +0530

    User logout implemented

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:todoapp/screens/login_screen.dart';
+import 'package:todoapp/sevices/auth_services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +11,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return const Text("Home screen");
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Home Screen"),
+          actions: [
+            ElevatedButton.icon(
+                onPressed: () async {
+                  await _auth.signOut();
+                  print(
+                      "============================================== user loged out");
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
+                },
+                label: Icon(Icons.logout))
+          ],
+        ),
+        body: const Text("Helloooo"));
   }
 }
